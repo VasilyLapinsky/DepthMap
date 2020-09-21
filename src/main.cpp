cfc3d5198c952607f23ab5f2f79e9d987b49d56d
VasilyLapinsky <vasia2001lapa3251@gmail.com>
2020-09-21 18:42:59 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include "disparitymap.h"
+
+void videoCameraTest()
+{
+	VideoCapture cap(1);
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
+		return;
+	}
+	StereoImagePreprocessor preProcessor(COLOR_BGR2GRAY, 0.5, 0.5, INTER_LINEAR_EXACT);
+	DisparityMapMaker maker(32, 15);
+	DisparityMapWriter writer("Disparity map", CV_8UC1, COLORMAP_AUTUMN);
+
+	Mat image;
+	for (;;) {
+		cap >> image;
+		preProcessor.process(image);
+		maker.compute(preProcessor.getleft(), preProcessor.getright());
+		writer.show(maker.getDisparityMap());
+		if (waitKey(33) >= 0) break;
+	}
+}
+
+int main()
+{
+
+	return 0;
+}
